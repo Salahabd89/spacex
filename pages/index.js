9d0f273426b3457d965510fb51d122b7cf6c7a29
System Administrator <root@192-168-1-103.tpgi.com.au>
2020-09-27 17:53:31 +1000

    prod optimisation

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
-      <style jsx global>{`
-        background: red;
-      `}</style>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
